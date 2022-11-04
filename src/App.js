8cc8179a862cafa6600293998003f6c00733b57b
Josip Prpić <47066785+josippr@users.noreply.github.com>
2022-11-04 23:51:28 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Components/header';
+import Main from './Components/main';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Main></Main>
+    </div>
+  );
+}
+
+export default App;
