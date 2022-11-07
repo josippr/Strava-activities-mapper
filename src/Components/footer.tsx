4cae0cf0f974d7b0b4e3093694c64ffcbbff4208
Josip Prpić <47066785+josippr@users.noreply.github.com>
2022-11-07 23:38:16 +0100

    Global footer

---
+++ src/Components/footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../App.css';
+import './header.css';
+import './footer.css';
+
+function Footer() {
+  return (
+    <div className="footer">
+      
+    </div>
+  );
+}
+
+export default Footer;
